054317c56dc13ccbf09989d0a9bd642010c50bdb
Nabellaleen <Nabellaleen@users.noreply.github.com>
2014-10-19 10:43:02 +0200

    Add categories mechanisms

--- mylittlebudget/core.py
+++ mylittlebudget/core.py
@@ -16,7 +16,8 @@ class User:
 class Entry:
     """docstring for Entry"""
     def __init__(self, name, category, date, value, count):
-        # TODO : manage value equals to zero
+        if value == 0:
+            raise Exception('Value cannot be zero')
         self.name = name
         self.category = category
         self.date = date
@@ -48,32 +49,81 @@ class Account:
     """TODO"""
     _entries = []
 
-    def __init__(self, name, users):
+    def __init__(self, name, users, categories=None):
         self.name = name
         self.users = users
+        self.categories = {}
+        if categories:
+            self.categories = {category.name: category
+                               for category in categories}
 
-    def add_expense(self, name, category, value, date=datetime.today(), count=1):
-        new_value = value if value < 0 else -value
-        new_entry = Entry(name, category, date, new_value, count)
+    def add_expense(self, name, category, total, date=datetime.today(),
+                    count=1):
+        new_category = self.get_category(category)
+        if not new_category:
+            exception_message = '{0} not previously defined in {1}'.format(
+                category, self)
+            raise Exception(exception_message)
+        new_total = total if total < 0 else -total
+        new_entry = Entry(name, new_category, date, new_total, count)
         self._entries.append(new_entry)
 
     def get_expenses(self):
         return [entry for entry in self._entries if entry.is_expense()]
 
+    def add_category(self, category):
+        self.categories[category.name] = category
+
+    def get_category(self, category):
+        if isinstance(category, Category):
+            # TODO : optimize with directly searching the object in the values
+            return self.categories.get(category.name)
+        elif isinstance(category, str):
+            return self.categories.get(category)
+        else:
+            raise Exception('Not handled parameter : {0}'.format(category))
+
+    def __str__(self):
+        return '{0} account'.format(self.name)
+
+
+class Category(object):
+    """docstring for Category"""
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return 'category {0}'.format(self.name)
+
 
 def main():
+    categories_names = ['Vehicle', 'Food']
+    categories = {name: Category(name=name) for name in categories_names}
+
+    vehicle_category = categories['Vehicle']
+    food_category = categories['Food']
+    beauty_category = Category(name='Beauty')
+    ######################
+    # Remove in final test
+    categories[beauty_category.name] = beauty_category
+    ######################
+
     user_names = ['Héloïse', 'Florian']
     users = [User(name=name) for name in user_names]
     current_account = Account(name='household',
-                              users=users)
-    current_account.add_expense(name='Fuel', category='Vehicle',
-                                value=22)
-    current_account.add_expense(name='Restaurant', category='Food',
-                                value=14)
+                              users=users,
+                              categories=categories.values())
+    current_account.add_expense(name='Fuel', category=vehicle_category,
+                                total=22)
+    current_account.add_expense(name='Restaurant', category=food_category,
+                                total=14)
     current_account.add_expense(name='Milk', category='Food',
-                                value=4, count=4)
-    current_account.add_expense(name='Bread', category='Food',
-                                value=1, count=2)
+                                total=4, count=4)
+    current_account.add_expense(name='Bread', category=food_category,
+                                total=1, count=2)
+    current_account.add_expense(name='Soap', category=beauty_category,
+                                total=12, count=2)
 
     current_expenses = current_account.get_expenses()
     for expense in current_expenses:
