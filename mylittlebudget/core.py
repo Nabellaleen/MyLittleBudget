fe6cb975dc4bb33663db563ba38a57e795ef9a87
Nabellaleen <Nabellaleen@users.noreply.github.com>
2014-11-28 12:20:39 +0100

    Entry value : use an overridable class instead of a number

--- mylittlebudget/core.py
+++ mylittlebudget/core.py
@@ -19,41 +19,91 @@ class User:
         return 'User({0}, {1})'.format(self.name, self.share_index)
 
 
+class Entry_Value:
+    """TODO"""
+    type_id = None
+    label = None
+
+    def __init__(self, value):
+        if not isinstance(value, Number):
+            raise TypeError('value parameter {0} is not a Number'.format(value))
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+    def get_type(self):
+        """ Return type of the value, as a string. By default, if `type_id` is
+            not overrided, the type is the class name as lowercase
+        """
+        if self.type_id is not None:
+            return self.type_id
+        return self.__class__.__name__.lower()
+
+    def get_label(self):
+        """ Return label of the value, as a string. By default, if `label` is
+            not overrided, the type is the class name, ith _ replaced by spaces
+            and with title case
+        """
+        if self.label is not None:
+            return self.label
+        return self.__class__.__name__.replace('_', ' ').title()
+
+    def get_value(self):
+        return self.value
+
+
+class Expense(Entry_Value):
+    """TODO"""
+
+    def __init__(self, value):
+        value = -abs(value)
+        super().__init__(value)
+
+
+class Credit(object):
+    """docstring for Credit"""
+
+    def __init__(self, value):
+        value = abs(value)
+        super().__init__(value)
+
+
 class Entry:
     """docstring for Entry"""
-    def __init__(self, name, category, date, value, count, users):
-        if value == 0:
-            raise Exception('Value cannot be zero')
+    def __init__(self, name, category, date, value, users, count=1):
         self.name = name
         self.category = category
         self.date = date
+        if not isinstance(value, Entry_Value):
+            raise TypeError('value parameter ({0}) is not an Entry_Value'
+                .format(value))
         self.value = value
         self.count = count
         self.users = users
 
     def __str__(self):
-        entry_type = 'Expense' if self.is_expense() else 'Credit'
+        entry_type = self.value.get_label()
         entry_type_str = '{0:%Y-%m-%d} - {1} in {2}'.format(self.date,
                                                             entry_type,
                                                             self.category)
         if self.count > 1:
-            entry_item_str = '{0} {1} at {2:n}€'.format(self.count,
-                                                        self.name,
-                                                        abs(self.value))
+            entry_item_str = self.__get_multi_count_entry_item_str()
         else:
-            entry_item_str = '{0} at {1:n}€'.format(self.name,
-                                                    abs(self.value))
+            entry_item_str = self.__get_single_count_entry_item_str()
         users_str = [str(user) for user in self.users]
         entry_users_str = 'Users : ' + ', '.join(users_str)
         return '{0}: {1} - {2}'.format(entry_type_str, entry_item_str,
                                        entry_users_str)
 
-    def is_expense(self):
-        return self.value < 0
+    def __get_multi_count_entry_item_str(self):
+        return '{0} {1} at {2:n}€'.format(self.count,
+                                          self.name,
+                                          abs(self.value.get_value()))
 
-    def is_credit(self):
-        return self.value > 0
-
+    def __get_single_count_entry_item_str(self):
+        return '{0} at {1:n}€'.format(self.name,
+                                      abs(self.value.get_value()))
 
 class Account:
     """TODO"""
@@ -71,31 +121,37 @@ class Account:
     def total_share_index(self):
         return sum([user.share_index for user in self.users])
 
-    def add_expense(self, name, category, total, users=None,
-                    date=datetime.today(), count=1):
+    def add_expense(self, name, category, value, users=None,
+                    date=datetime.today()):
         new_category = self.get_category(category)
         if not new_category:
             exception_message = '{0} not previously defined in {1}'.format(
                 category, self)
             raise Exception(exception_message)
-        new_total = total if total < 0 else -total
         new_users = users if users is not None else self.users
-        new_entry = Entry(name, new_category, date, new_total, count, new_users)
+        new_entry = Entry(name, new_category, date, value, new_users)
         self._entries.append(new_entry)
 
     def get_expenses(self):
-        return [entry for entry in self._entries if entry.is_expense()]
+        return [entry for entry in self._entries if isinstance(entry.value,
+                                                               Expense)]
 
-    def get_expenses_by_user(self):
+    def get_expenses_by_user(self, date_min=None, date_max=None):
+        return self.get_entries_by_user(entry_filter=[Expense],
+                                        date_min=date_min, date_max=date_max)
+
+    def get_entries_by_user(self, entry_filter=[],
+                            date_min=None, date_max=None):
         expenses = []
         total_share_index = self.total_share_index
         for entry in self._entries:
-            if not entry.is_expense():
+            if entry_filter and entry.value.__class__ not in entry_filter:
                 continue
             expense = {'entry': entry}
             expenses.append(expense)
+            entry_value = entry.value.get_value()
             for user in entry.users:
-                user_share = entry.value * user.share_index / total_share_index
+                user_share = entry_value * user.share_index / total_share_index
                 expense[user.name] = user_share
         return expenses
 
@@ -153,15 +209,15 @@ def main():
                               users=users,
                               categories=categories.values())
     current_account.add_expense(name='Fuel', category=vehicle_category,
-                                total=22)
+                                value=Expense(22))
     current_account.add_expense(name='Restaurant', category=food_category,
-                                total=14)
+                                value=Expense(14))
     current_account.add_expense(name='Milk', category='Food',
-                                total=4, count=4, users=users[:1])
+                                value=Expense(4), users=users[:1])
     current_account.add_expense(name='Bread', category=food_category,
-                                total=1, count=2, users=users[1:2])
+                                value=Expense(1), users=users[1:2])
     current_account.add_expense(name='Soap', category=beauty_category,
-                                total=12, count=2)
+                                value=Expense(12))
 
     current_expenses = current_account.get_expenses()
     for expense in current_expenses:
@@ -176,7 +232,7 @@ def main():
     for expense in current_expenses_by_user:
         entry = expense['entry']
         standard_row = [entry.name, entry.date]
-        users_row = [expense.get(user) or '' for user in users_columns]
+        users_row = [abs(expense.get(user) or 0) or '' for user in users_columns]
         total_row = [total_sum(x, y) for x, y in zip(total_row, users_row)]
         expenses_table.add_row(standard_row + users_row)
     empty_row = ['' for i in expenses_table.field_names]
